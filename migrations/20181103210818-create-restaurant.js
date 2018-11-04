8403ecf2e6dcca63b3f83ea1d3f1ddcb62372ee3
Victoria Sedano <vicki.sedano@gmail.com>
2018-11-04 14:09:19 -0600

    Database connected and Restaurant model created

---
+++ migrations/20181103210818-create-restaurant.js
@@ -0,0 +1,67 @@
+const uuid = require('uuid/v1')
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Restaurants', {
+      id: {
+        allowNull: false,
+        type: Sequelize.UUID,
+        primaryKey: true
+      },
+      rating: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      },
+      name: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        notEmpty: true
+      },
+      site: {
+        type: Sequelize.TEXT,
+      },
+      email: {
+        type: Sequelize.TEXT,
+      },
+      phone: {
+        type: Sequelize.TEXT
+      },
+      street: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        notEmpty: true
+      },
+      city: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        notEmpty: true
+      },
+      state: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        notEmpty: true
+      },
+      lat: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        notEmpty: true
+      },
+      lng: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        notEmpty: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Restaurants');
+  }
+};
---
+++ models/restaurant.js
@@ -0,0 +1,104 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Restaurant = sequelize.define('Restaurant', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      validate: { 
+        min: {
+          args: [0],
+          msg: 'El valor de Rating es de 0-4, ingresa un valor mayor'},
+        max: {
+          args: 4,
+          msg: 'El valor de Rating es de 0-4, ingresa un valor menor'}
+      }
+    },
+    name: {
+      type:DataTypes.TEXT,
+      allowNull: false,
+      defaultValue: '',
+      validate: {
+        notEmpty: {
+          args: true, 
+          msg: 'Se requiere un nombre del restaurant'}
+      }
+    },
+    site: {
+      type:DataTypes.TEXT,
+      validate: {
+        isUrl: {
+          args:true,
+          msg: 'La dirección web que ingresaste no es válida.'
+        }
+      }
+    },
+    email: {
+      type:DataTypes.TEXT,
+      validate: {
+        isEmail: {
+          args:true,
+          msg: 'El correo electrónico no es válido'
+        }
+      }
+    },
+    phone: DataTypes.TEXT,
+    street: {
+      type:DataTypes.TEXT,
+      allowNull: false,
+      defaultValue: '',
+      validate: {
+        notEmpty: {
+          args: true, 
+          msg: 'Se requiere una calle del restaurant'}
+      }
+    },
+    city: {
+      type:DataTypes.TEXT,
+      allowNull: false,
+      defaultValue: '',
+      validate: {
+        notEmpty: {
+          args: true, 
+          msg: 'Se requiere una ciudad del restaurant'}
+      }
+    },
+    state: {
+      type:DataTypes.TEXT,
+      allowNull: false,
+      defaultValue: '',
+      validate: {
+        notEmpty: {
+          args: true, 
+          msg: 'Se requiere un estado (ubicación) del restaurant'}
+      }
+    },
+    lat: {
+      type:DataTypes.FLOAT,
+      allowNull: false,
+      defaultValue: '',
+      validate: {
+        notEmpty: {
+          args: true, 
+          msg: 'Se requiere la latitud del restaurant'}
+      }
+    },
+    lng: {
+      type:DataTypes.FLOAT,
+      allowNull: false,
+      defaultValue: '',
+      validate: {
+        notEmpty: {
+          args: true, 
+          msg: 'Se requiere la longitud del restaurant'}
+      }
+    },
+  }, {});
+  Restaurant.associate = function(models) {
+    // associations can be defined here
+  };
+  return Restaurant;
+};
